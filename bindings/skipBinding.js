0a5e07ff167547d6a17132379bf72df292fca41d
Tatjana Kopalova <tatjana19@users.noreply.github.com>
2016-08-04 15:30:52 +0200

    Update skipBinding.js

--- bindings/skipBinding.js
+++ bindings/skipBinding.js
@@ -1,4 +1,8 @@
-ko.bindingHandlers.skipBinding = {
+/**
+ * Tell Knockout to Skip Binding.
+ * @see http://www.knockmeout.net/2012/05/quick-tip-skip-binding.html
+ **/
+ ko.bindingHandlers.skipBinding = {
     init: function() {
         return { controlsDescendantBindings: true };
     }
