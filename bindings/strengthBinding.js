06b2bb4662ce315ecd7e7ca5838abdb428b06cbd
Tatjana Kopalova <tatjana19@users.noreply.github.com>
2016-08-09 17:00:02 +0200

    Update strengthBinding.js

--- bindings/strengthBinding.js
+++ bindings/strengthBinding.js
@@ -1,1 +1,10 @@
-/**/
+/**
+ * Binding for the jquery plugin 'strength.js'(http://jquerycards.com/forms/inputs/strength-js/), so if you want to use this binding you have to include the required js files for this
+ * jquery plugin.
+ **/
+ ko.bindingHandlers.strength = { 
+	init: function(element, valueAccessor) { 
+		var options = valueAccessor() || {}; 
+		$(element).strength(options); 
+	} 
+};
