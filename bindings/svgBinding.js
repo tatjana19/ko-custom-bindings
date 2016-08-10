2d4bbc228f54d1753f7422b3f03f7d0ceb2de87d
Tatjana Kopalova <tatjana19@users.noreply.github.com>
2016-08-10 15:51:05 +0200

    Update svgBinding.js

--- bindings/svgBinding.js
+++ bindings/svgBinding.js
@@ -1,3 +1,4 @@
 /** 
- * http://keith-wood.name/svg.html => SVG for jQuery v1.5.0.
+ * Binding for the jquery plugin 'jquery.svg.js' => http://keith-wood.name/svg.html => SVG for jQuery v1.5.0.
+ * If you want to use this binding you have to include the required js files.
  **/
