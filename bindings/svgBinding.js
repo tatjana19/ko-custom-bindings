e04045e8d504dd000f233b20336957a8d825c466
Tatjana Kopalova <tatjana19@users.noreply.github.com>
2016-08-10 15:53:26 +0200

    Update svgBinding.js

--- bindings/svgBinding.js
+++ bindings/svgBinding.js
@@ -2,3 +2,9 @@
  * Binding for the jquery plugin 'jquery.svg.js' => http://keith-wood.name/svg.html => SVG for jQuery v1.5.0.
  * If you want to use this binding you have to include the required js files.
  **/
+ ko.bindingHandlers.jqsvg = { 
+	init: function(element, valueAccessor) { 
+		var options = valueAccessor() || {}; 
+		$(element).svg(options); 
+	} 
+};
